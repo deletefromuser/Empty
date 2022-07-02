25526d2d8f75814fc88ddcb324c5ce4c22766300
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2022-07-02 18:15:48 +0800

    init

---
+++ app/src/main/java/com/example/empty/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.empty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        Toast.makeText(this, "send button clicked", Toast.LENGTH_SHORT).show()
+//        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+//        val message = editText.text.toString()
+//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+//            putExtra(EXTRA_MESSAGE, message)
+//        }
+//        startActivity(intent)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+learn android develop from start again
